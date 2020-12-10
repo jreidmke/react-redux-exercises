984838ae8f057ed81a106dae2a12aef3344ad420
unknown <jreidmke@gmail.com>
2020-12-10 11:48:03 -0600

    init Meme component and Images/

---
+++ meme/src/Meme.js
@@ -0,0 +1,11 @@
+import MemeForm from './MemeForm';
+import './Meme.css';
+
+// const Meme = ({imgSrc, topTxt, btmTxt}) => {
+//     return(
+//         <div>
+//             <h3>HELLO</h3>
+//             <img sr
+//         </div>
+//     )
+// }
