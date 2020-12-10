4cf3be8877edb6d8ec01bf2c42a9a512c0104df2
unknown <jreidmke@gmail.com>
2020-12-10 12:11:01 -0600

    completed first gen meme component

--- meme/src/Meme.js
+++ meme/src/Meme.js
@@ -1,11 +1,15 @@
-import MemeForm from './MemeForm';
+// import MemeForm from './MemeForm';
 import './Meme.css';
+import infinity_train from './Images/infinity_train_book_3.jpg';
 
-// const Meme = ({imgSrc, topTxt, btmTxt}) => {
-//     return(
-//         <div>
-//             <h3>HELLO</h3>
-//             <img sr
-//         </div>
-//     )
-// }
+const Meme = ({imgSrc, topTxt, btmTxt}) => {
+    return(
+        <div id='memeContainer'>
+            <h3 id='topTxt'>{topTxt}</h3>
+            <img src={imgSrc} alt='meme-img' id='meme-img'/>
+            <h3 id='btmTxt'>{btmTxt}</h3>
+        </div>
+    )
+}
+
+export default Meme;
