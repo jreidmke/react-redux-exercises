301ebbf5d2446340f9e2e4b718976896b2d935b2
unknown <jreidmke@gmail.com>
2020-12-10 12:10:15 -0600

    imported Meme component

--- meme/src/App.js
+++ meme/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Meme from './Meme';
+import infinity_train from './Images/infinity_train_book_3.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Meme imgSrc={infinity_train} topTxt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." btmTxt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
     </div>
   );
 }
